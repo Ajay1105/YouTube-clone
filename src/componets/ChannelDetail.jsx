c3d3c5c64a9c56b4cd775e1fd9db83f0b8542ccf
Ajay <aky11052003@gmail.com>
2022-10-24 18:38:18 +0530

    channel page completed

--- src/componets/ChannelDetail.jsx
+++ src/componets/ChannelDetail.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import {Box} from '@mui/material'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
-import {Videos, ChannelCard} from './index'
-import { fetchFromApi } from '../utils/fetchFromApi'
+import { Videos, ChannelCard } from "./index";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 function ChannelDetail() {
-  const {id} = useParams();
-  let a= [];
-  fetchFromApi(`channels?part="snippet&id=${id}`)
-  .then((data)=>a=data?.items[0]);
-  const [channelDetail, setChannelDetail] = useState(a)
- 
-useEffect(()=>{
-  fetchFromApi(`channels?part="snippet&id=${id}`)
-  .then((data)=>setChannelDetail(data?.items[0]));
-},[id])
-console.log(channelDetail);
+
+  const { id } = useParams();
+  
+  // let a = [];
+  // fetchFromApi(`channels?part=snippet&id=${id}`).then(
+  //   (data) => (a = data?.items[0])
+  // );
+
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  //console.log(channelDetail,videos);
 
   return (
-    <div>{id}</div>
-  )
+    <>
+      <ChannelCard channelDetail={channelDetail} />
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        {videos.map((item) => (
+          <Videos videos={videos} />
+        ))}
+      </Box>
+    </>
+  );
 }
 
-export default ChannelDetail
+export default ChannelDetail;
