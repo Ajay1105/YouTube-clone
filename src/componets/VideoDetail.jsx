c2df9674b98db0920a011b91a4477e27094bd0eb
Ajay <aky11052003@gmail.com>
2022-10-25 15:23:29 +0530

    dotenv

--- src/componets/VideoDetail.jsx
+++ src/componets/VideoDetail.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import { Link, useParams } from "react-router-dom";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-function VideoDetail() {
-  return (
-    <div>VideoDetail</div>
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { Videos } from "./index";
+
+const VideoDetail = () => {
+ const { id } = useParams();
+ 
+  let a = [];
+  fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
+  .then((data) => a= data)
+ 
+ const [videoDetail, setVideoDetail] = useState(a);
+
+ useEffect( async() => {
+  await fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
+  .then((data) =>{ console.log(data);
+    setVideoDetail(data.items[0])});
+ }, [id]);
+
+  return(
+  <Box minHeight="95vh">
+   <Stack direction={{sx:'column',md:'row'}}>
+    <Box flex={1}>
+     <Box sx={{width:'100%'}}>
+      <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
+      <Typography color="#fff" variant="h5" fontWeight="bold">
+      Title
+      </Typography>
+     </Box>
+    </Box>
+   </Stack>
+  </Box>
   )
-}
+};
 
-export default VideoDetail
+export default VideoDetail;
